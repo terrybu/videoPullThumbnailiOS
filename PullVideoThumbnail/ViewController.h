4e0310f2fd6ebba93c7e6f44ea6046844f5469d5
Terry Bu <burea1124@gmail.com>
2015-03-27 15:51:45 -0400

    Initial Commit

---
+++ PullVideoThumbnail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PullVideoThumbnail
+//
+//  Created by Terry Bu on 3/27/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
